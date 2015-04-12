548efa81ab6aef0e241093f648098344c6bdcbe9
Adam Strzelecki <ono@java.pl>
2015-04-12 16:48:30 +0200

    Nim AsyncHTTPServer

---
+++ nim_asynchttpserver.nim
@@ -0,0 +1,8 @@
+import asynchttpserver, asyncdispatch
+
+var server = newAsyncHttpServer()
+proc cb(req: Request) {.async.} =
+  await req.respond(Http200, "Hello World")
+
+asyncCheck server.serve(Port(8080), cb)
+runForever()
