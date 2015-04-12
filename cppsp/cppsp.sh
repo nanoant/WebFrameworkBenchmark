a698d15a53d23f75a11c9517c91c045503a8c7a8
Adam Strzelecki <ono@java.pl>
2015-04-12 17:29:16 +0200

    Split & add undertow

---
+++ cppsp/cppsp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+[ -d download ] || mkdir download
+
+[ -f download/cppsp_1.0.tar.xz ] || \
+	curl -o download/cppsp_1.0.tar.xz 'http://cznic.dl.sourceforge.net/project/cpollcppsp/CPPSP%201.0/cppsp_1.0.tar.xz'
+
+[ -d download/cppsp_1.0 ] || \
+	tar xf download/cppsp_1.0.tar.xz -C download
