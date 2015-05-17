bb4f289eea9766c03ebf2b0412d82f8e0f0ca277
Adam Strzelecki <ono@java.pl>
2015-05-17 19:25:23 +0200

    Fix OSX threads count

--- common/threads.sh
+++ common/threads.sh
@@ -1,7 +1,7 @@
 if [ -f /proc/cpuinfo ]; then
 	threads=$(cat /proc/cpuinfo | grep -i 'core id' | wc -l)
 elif [ -d /System/Library ]; then
-	threads=$(sysctl machdep.cpu.thread_count)
+	threads=$(sysctl machdep.cpu.thread_count | cut -f2 -d\ )
 else
 	threads=4
 fi
