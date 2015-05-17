a2c57fd1665daf35ae8646140592e9208b1bf41e
Adam Strzelecki <ono@java.pl>
2015-05-17 19:22:47 +0200

    Onion: Fix for Linux tests

--- onion/run.sh
+++ onion/run.sh
@@ -7,7 +7,8 @@ if [ ! -x examples/hello/hello ]; then
 	if [ ! -f ../libev/.libs/libev.a ]; then
 		( cd ../libev && configure && make -j8 )
 	fi
-	cmake -GNinja -DLIBEV_HEADER=$PWD/../libev -DLIBEV_LIB=$PWD/../libev/.libs/libev.a
+	cmake -GNinja -DLIBEV_HEADER=$PWD/../libev -DLIBEV_LIB=$PWD/../libev/.libs/libev.a -DGNUTLS_LIB=/dev/null && \
+	ninja examples/hello/hello
 fi
 
 ONION_LOG=noinfo exec examples/hello/hello
