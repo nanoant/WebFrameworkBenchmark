ec2259d8fafa49c178b02979d31fed4a0beb50c2
Adam Strzelecki <ono@java.pl>
2015-12-21 19:29:33 +0100

    Go: fasthttp pre-fork reuse addr version

--- benchmarks/go-fasthttp/helloworldserver.go
+++ benchmarks/go-fasthttp/helloworldserver.go
@@ -4,19 +4,68 @@ import (
 	"flag"
 	"io"
 	"log"
+	"net"
+	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/reuseport"
 )
 
-var addr = flag.String("addr", ":8080", "TCP address to listen to")
+var (
+	addr    = flag.String("addr", ":8080", "TCP address to listen to")
+	prefork = flag.Bool("prefork", false, "use prefork")
+	child   = flag.Bool("child", false, "is child proc")
+)
 
 func main() {
 	flag.Parse()
-	if err := fasthttp.ListenAndServe(*addr, requestHandler); err != nil {
+
+	ln := getListener()
+
+	if err := fasthttp.Serve(ln, requestHandler); err != nil {
 		log.Fatalf("Error in ListenAndServe: %s", err)
 	}
 }
 
 func requestHandler(ctx *fasthttp.RequestCtx) {
 	io.WriteString(ctx, "Hello World")
+}
+
+func getListener() net.Listener {
+	if !*prefork {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		ln, err := net.Listen("tcp4", *addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return ln
+	}
+
+	if !*child {
+		children := make([]*exec.Cmd, runtime.NumCPU())
+		for i := range children {
+			children[i] = exec.Command(os.Args[0], "-prefork", "-child")
+			children[i].Stdout = os.Stdout
+			children[i].Stderr = os.Stderr
+			if err := children[i].Start(); err != nil {
+				log.Fatal(err)
+			}
+		}
+		for _, ch := range children {
+			if err := ch.Wait(); err != nil {
+				log.Print(err)
+			}
+		}
+		os.Exit(0)
+		panic("unreachable")
+	}
+
+	runtime.GOMAXPROCS(1)
+	ln, err := reuseport.Listen("tcp4", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ln
 }
